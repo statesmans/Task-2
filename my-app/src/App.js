3cbf566d7fce194ccbee817da8dbea5f886e98ea
statesmans <siroshtan.yegor@gmail.com>
2021-07-04 14:30:05 +0300

    delete useless css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { NoteBlockContainer } from './app/components/notes/NoteBlockContainer';
+import s from './app/components/Main.module.scss';
 import StatisticsBlock from './app/components/statistics/StatisticsBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={s.App}>
         <NoteBlockContainer/>
         <StatisticsBlock/>
-      </header>
     </div>
   );
 }
